51bf6dab3483f5cd5e37a6c5b53593c28af24c88
vastuna-7733 <vastuna7733@gmail.com>
2025-09-28 21:32:22 +0530

    Update Helloworld2

--- src/com/sree/Helloworld2.java
+++ src/com/sree/Helloworld2.java
@@ -4,7 +4,7 @@ public class Helloworld2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello world - Team");
+		System.out.println("Hello world2 - Team");
 		
 
 	}
